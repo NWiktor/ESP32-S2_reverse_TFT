2dd4141b552e2172a5b427bdd4bf6d2ed999acb2
NWiktor <31969508+NWiktor@users.noreply.github.com>
2023-05-30 19:27:26 +0200

    fix text

--- esp32-s2-rev-tft/soft_boot.py
+++ esp32-s2-rev-tft/soft_boot.py
@@ -13,12 +13,12 @@ def main():
     mach_name = os.uname().machine
     sys_name = os.uname().sysname
 
-    start_text = f"Hello, World!\n{mach_name}\nSystem: {sys_name}"
+    start_text = f"Hello, World!\n{mach_name[:25]}\n{mach_name[26:]}\nSystem: {sys_name}"
     text_area = bitmap_label.Label(terminalio.FONT, text=start_text, scale=1, color=0xFF00FF)
     text_area.x = 10
     text_area.y = 10
     board.DISPLAY.show(text_area)
-    time.sleep(5)
+    time.sleep(10)
 
 
 if __name__ == '__main__':
