616f7c1d46d42a4de6e575a43802f4cb274898da
NWiktor <31969508+NWiktor@users.noreply.github.com>
2023-06-15 21:09:54 +0200

    pylinter cleanup, + add wait for booting

--- esp32-s2-rev-tft/soft_boot.py
+++ esp32-s2-rev-tft/soft_boot.py
@@ -1,6 +1,7 @@
 """ This module contains soft boot related functions. """
 
 # pylint: disable = import-error
+# pylint: disable = no-member
 import os
 import time
 import board
@@ -13,13 +14,15 @@ def main():
     mach_name = os.uname().machine
     sys_name = os.uname().sysname
 
-    start_text = f"Hello, World!\n{mach_name[:25]}\n{mach_name[26:]}\nSystem: {sys_name}"
+    start_text = (f"Hello, World!\n{mach_name[:25]}\n{mach_name[26:]}\n"
+    + f"System: {sys_name}\nInitializing...")
     text_area = bitmap_label.Label(terminalio.FONT, text=start_text, scale=1, color=0xFF00FF)
     text_area.x = 10
     text_area.y = 10
     board.DISPLAY.show(text_area)
-    time.sleep(2)
-
+    ####
+    time.sleep(2) # Important for powering up Feathers properly!
+    ####
 
 if __name__ == '__main__':
     pass
